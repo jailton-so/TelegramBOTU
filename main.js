fc0585877b7ed5ed6fdb8d0095b28818d8e89e7a
Jailton Silva <jailtonsilva.olv@gmail.com>
2023-04-05 01:08:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -16,11 +16,12 @@ const https = require('node:https')
 const fs = require('node:fs')
 const port = 443
 
-const telegramBotTokken = '6076259682:AAEwFW9MwcwDGID0yXIeiKoR6ctNeDzx16k'
+const telegramBotToken = '6076259682:AAEwFW9MwcwDGID0yXIeiKoR6ctNeDzx16k'
 const telegramApiHost = 'api.telegram.org'
-const telegramApiPath = '/bot'+telegramBotTokken
-const webhookHost = 'example.com'
+const telegramApiPath = '/bot'+telegramBotToken
+const webhookHost = '68ed-2804-29b8-509b-d613-391e-a2db-3774-1492.sa.ngrok.io'
 const webhookPath = '/webhook'
+const webhookSecretToken = 'mySecretToken123'
 
 serverOptions = {
   key: fs.readFileSync('ssl/privateKey.pem'),
@@ -29,24 +30,35 @@ serverOptions = {
 }
 
 const server = https.createServer(serverOptions, (req, res) => {
-  requestHandler(req, res)
+  console.log('requested to: '+req.url)
+  if(req.url === '/'){
+    res.writeHead(200);res.write('<h1>Hello, world</h1>');res.end()
+  }else if(req.url === webhookPath){
+    webhookHandler(req, res)
+  }else{
+    res.writeHead(404);res.write('<h1>404, not found</h1>');res.end()
+  }
 })
 server.listen(port);console.log('listening on port '+port+'...')
-server.on('error', (error)=>{
-  console.log('server error: '+error.message)
-})
+server.on('error', (error)=>{console.log('server error: '+error.message)})
 
-function requestHandler(req, res){
-  if(req.url === '/'){
-    console.log('requested to: '+req.url)
-    res.writeHead(200)
-    res.write('<h1>Hello, world</h1>')
-    res.end()
+
+function webhookHandler(req, res){
+  if(req.headers['x-telegram-bot-api-secret-token'] !== webhookSecretToken){
+    console.log('invalid secret token')
   }else{
-    console.log('requested to: '+req.url+' -> not found')
-    res.writeHead(404)
-    res.write('<h1>404, not found</h1>')
-    res.end()
+    let rawData = ''
+    req.on('data', (chunk)=>{
+      rawData+=chunk
+    })
+    req.on('end', ()=>{
+      console.log('webhookHandler() request info: ')
+      // console.log(JSON.parse(rawData))
+      console.log(rawData)
+      res.writeHead(200)
+      res.end()
+
+    })
   }
 }
 
@@ -65,12 +77,14 @@ function getWebhookInfo(){
 function setWebhook(){
   const reqOptions = {
     hostname: telegramApiHost,
-    path: telegramApiPath+'/setwebhook?url='+webhookHost+webhookPath
+    path: telegramApiPath+'/setwebhook?url='+webhookHost+webhookPath+'&secret_token='+webhookSecretToken+'&drop_pending_updates=true',
+    method: 'POST',
   }
   https.request(reqOptions, (res)=>{
     let rawData = ''
     res.on('data', (chunk) => {rawData += chunk})
-    res.on('end', () => {console.log('setWebhook() response:\n\t'+rawData); getWebhookInfo()})
+    res.on('end', () => {console.log('setWebhook() response:\n\t'+rawData);
+    getWebhookInfo()})
   }).end().on('error', (error) => {console.log('setWebhook() request error: '+error.message)})
 }
 setWebhook()
